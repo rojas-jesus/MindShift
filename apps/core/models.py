0d1140712b46960157bfd3518739f06a6ab0b171
rojas-jesus <jesus.miguel.rojas@outlook.com>
2023-11-05 23:55:58 -0400

    feat: Add emotion attribute to Thought model

--- apps/core/models.py
+++ apps/core/models.py
@@ -1,10 +1,22 @@
 from django.db import models
 
+EMOTION_CHOICES = [
+    ("sad", "Sad"),
+    ("happy", "Happy"),
+    ("worry", "Worry"),
+    ("angry", "Angry"),
+]
+
+
 class Thought(models.Model):
     name = models.CharField(max_length=150)
     description = models.TextField()
-    advantages = models.TextField(null = True, blank = True)
-    disadvantages = models.TextField(null = True, blank = True)
+    advantages = models.TextField(null=True, blank=True)
+    disadvantages = models.TextField(null=True, blank=True)
+    emotion = models.CharField(
+        max_length=20, choices=EMOTION_CHOICES, null=True, blank=True
+    )
+
 
 class ThoughtDate(models.Model):
     timestamp = models.DateTimeField()
@@ -16,5 +28,3 @@ class ThoughtDate(models.Model):
 #    date = models.DateTimeField()
 #    duration = models.PositiveSmallIntegerField()
 #    thought = models.ForeignKey(Thought, on_delete=models.SET_NULL, null=True)
-
-
