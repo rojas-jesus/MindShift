7ac957881782ae65e606b169b3e7abce728870f8
rojas-jesus <jesus.miguel.rojas@outlook.com>
2023-12-08 08:23:25 -0400

    feat: Configure 'homepage' app urls

--- config/urls.py
+++ config/urls.py
@@ -20,5 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
 
+    path("", include("apps.homepage.urls")),
+
     path("core/", include("apps.core.urls")),
 ]
