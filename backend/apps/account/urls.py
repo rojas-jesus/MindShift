ba4a758c9fa6ea814591af72c1ab77dfc8b5e46a
rojas-jesus <jesus.miguel.rojas@outlook.com>
2024-03-31 10:08:46 -0400

    feat(backend): Add register_view

--- backend/apps/account/urls.py
+++ backend/apps/account/urls.py
@@ -3,8 +3,10 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from .views import register_view
 
 urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path("api/register/", register_view, name="register"),
 ]
--- backend/apps/account/views.py
+++ backend/apps/account/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.status import HTTP_201_CREATED, HTTP_400_BAD_REQUEST
+from .serializers import RegisterSerializer
 
-# Create your views here.
+@api_view(["POST"])
+def register_view(request):
+    serializer = RegisterSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=HTTP_201_CREATED)
+    return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
